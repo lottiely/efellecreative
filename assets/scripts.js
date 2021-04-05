49c3d337065ad865b569f35068e3d6a01b149494
lottiely <saialmario1@gmail.com>
2021-04-05 09:50:41 -0700

    Updated rating function to have a minimum of 3

--- assets/scripts.js
+++ assets/scripts.js
@@ -44,7 +44,10 @@ function cleanUp() {
 }
 
 function getRating() {
-  return Math.floor(Math.random() * 5) + 1;
+  const max = 5;
+  const min = 3;
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 function getContent() {
